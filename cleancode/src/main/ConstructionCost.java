8e7a1736973b8f84a476676777edfc4df4ea4b88
sirisha00 <55032030+sirisha00@users.noreply.github.com>
2020-07-20 03:29:53 +0530

    Create ConstructionCost.java

---
+++ cleancode/src/main/ConstructionCost.java
@@ -0,0 +1,19 @@
+
+public class ConstructionCost {
+
+	public int stdmat(int area) {
+		return area*1200;
+	}
+
+	public int abstdmat(int area) {
+		return area*1500;
+	}
+
+	public int hstdmat(int area) {
+		return area*1800;
+	}
+
+	public int fullaut(int area) {
+		return area*2500;
+	}
+}
